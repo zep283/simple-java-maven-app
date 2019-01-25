2a56439ad9497de814a4af1a554ab32cbff92a2c
Zachary Pollack <zachary.pollack@infostretch.com>
2019-01-25 12:03:52 -0800

    debugging code

--- auth.groovy
+++ auth.groovy
@@ -6,13 +6,16 @@ def auth(token) {
 }
 
 def newJob() {
+    println "start"
     def date = Calendar.getInstance()
     def time = date.get(Calendar.HOUR_OF_DAY)
     def jobName = "RemoteJob" + time
+    println "name set"
     def config = "curl"+ \
         "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@localhost:8080/job/MvnPipe/config.xml" + \
         "> config.xml"
     config.execute()
+    println "config generated."
     def job = "curl -X POST -H" + 
             "Content-Type:application/xml" + 
             "-d @config.xml" + 
