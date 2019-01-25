05bf479bffb0276ad0c8ae1ca6e51cb11b8ca4aa
Zachary Pollack <zachary.pollack@infostretch.com>
2019-01-25 12:23:56 -0800

    l

--- auth.groovy
+++ auth.groovy
@@ -15,17 +15,17 @@ def newJob() {
         "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@172.17.0.2:8080/job/MvnPipe/config.xml" +
         " > config.xml"
     config.execute()
-    Thread.start { System.err << config.err }
     config.waitFor()
-    println "config generated."
+    println config.err.text
+    println config.text
     def job = "curl -X POST -H " + 
             "Content-Type:application/xml " + 
             "-d @config.xml " + 
             "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@172.17.0.2:8080/createItem?name=${jobName}"
     job.execute()
-    Thread.start { System.err << job.err }
     job.waitFor()
-    println "Job created."
+    println job.err.text
+    println job.text
 }
 
 return [
