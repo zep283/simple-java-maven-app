de07afb0d2aed8473a486bf65d4d5b6fd9e5aa43
Zachary Pollack <zachary.pollack@infostretch.com>
2019-01-25 11:59:37 -0800

    added unique to name

--- auth.groovy
+++ auth.groovy
@@ -6,6 +6,9 @@ def auth(token) {
 }
 
 def newJob() {
+    def date = Calendar.getInstance()
+    def time = date.get(Calendar.HOUR_OF_DAY)
+    def jobName = "RemoteJob" + time
     def config = "curl"+ \
         "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@localhost:8080/job/MvnPipe/config.xml" + \
         "> config.xml"
@@ -13,7 +16,7 @@ def newJob() {
     def job = "curl -X POST -H" + 
             "Content-Type:application/xml" + 
             "-d @config.xml" + 
-            "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@localhost:8080/createItem?name=RemoteJob"
+            "http://zac:11ccec7b2c0a7ab6a8b1328c6dbb20a9fa@localhost:8080/createItem?name=${jobName}"
     job.execute()
     println "Job created."
 }
